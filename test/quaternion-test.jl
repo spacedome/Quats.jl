1ef0435a6af63fd274cea42a61f5a10adc84dd59
spacedome <julien@spacedome.tv>
2017-08-02 19:03:58 -0400

    Add more basic tests

--- test/quaternion-test.jl
+++ test/quaternion-test.jl
@@ -14,12 +14,22 @@
     @test quat(false,true,false,false) == im
     @test quat(false,false,true,false) == jm
     @test quat(false,false,false,true) == km
+    @test quat(1,0,0,0) == 1
     @test quat(0,1,0,0) == im
     @test quat(0,0,1,0) == jm
     @test quat(0,0,0,1) == km
+    @test quat(1.0,0.0,0.0,0.0) == 1
     @test quat(0.0,1.0,0.0,0.0) == im
     @test quat(0.0,0.0,1.0,0.0) == jm
     @test quat(0.0,0.0,0.0,1.0) == km
+    @test quat(-1,0,0,0) == -1
+    @test quat(0,-1,0,0) == -im
+    @test quat(0,0,-1,0) == -jm
+    @test quat(0,0,0,-1) == -km
+    @test quat(-1.0,0.0,0.0,0.0) == -1
+    @test quat(0.0,-1.0,0.0,0.0) == -im
+    @test quat(0.0,0.0,-1.0,0.0) == -jm
+    @test quat(0.0,0.0,0.0,-1.0) == -km
     @test im*jm ==  km == -jm*im 
     @test jm*im == -km == -im*jm
     @test jm*km ==  im == -km*jm
@@ -30,6 +40,7 @@
 
 end
 
+# These mirror tests from Complex.jl
 @testset "arithmetic" begin
     @testset for T in (Float16, Float32, Float64, BigFloat)
         t = true
@@ -37,35 +48,61 @@ end
         u = quat(T(+1.0), T(+1.0), T(+1.0), T(+1.0))
 
         @testset "add and subtract" begin
-            @test T(+0.0) + im + jm + km == quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0))
-            # @test isequal(T(+0.0) + im, Complex(T(+0.0), T(+1.0)))
-            # @test isequal(T(-0.0) + im, Complex(T(-0.0), T(+1.0)))
-            # @test isequal(T(+0.0) - im, Complex(T(+0.0), T(-1.0)))
-            # @test isequal(T(-0.0) - im, Complex(T(-0.0), T(-1.0)))
-            # @test isequal(T(+1.0) + im, Complex(T(+1.0), T(+1.0)))
-            # @test isequal(T(-1.0) + im, Complex(T(-1.0), T(+1.0)))
-            # @test isequal(T(+1.0) - im, Complex(T(+1.0), T(-1.0)))
-            # @test isequal(T(-1.0) - im, Complex(T(-1.0), T(-1.0)))
-            # @test isequal(im + T(+0.0), Complex(T(+0.0), T(+1.0)))
-            # @test isequal(im + T(-0.0), Complex(T(-0.0), T(+1.0)))
-            # @test isequal(im - T(+0.0), Complex(T(+0.0), T(+1.0)))
-            # @test isequal(im - T(-0.0), Complex(T(+0.0), T(+1.0)))
-            # @test isequal(im + T(+1.0), Complex(T(+1.0), T(+1.0)))
-            # @test isequal(im + T(-1.0), Complex(T(-1.0), T(+1.0)))
-            # @test isequal(im - T(+1.0), Complex(T(-1.0), T(+1.0)))
-            # @test isequal(im - T(-1.0), Complex(T(+1.0), T(+1.0)))
-            # @test isequal(T(f) + im, Complex(T(+0.0), T(+1.0)))
-            # @test isequal(T(t) + im, Complex(T(+1.0), T(+1.0)))
-            # @test isequal(T(f) - im, Complex(T(+0.0), T(-1.0)))
-            # @test isequal(T(t) - im, Complex(T(+1.0), T(-1.0)))
-            # @test isequal(im + T(f), Complex(T(+0.0), T(+1.0)))
-            # @test isequal(im + T(t), Complex(T(+1.0), T(+1.0)))
-            # @test isequal(im - T(f), Complex(T(+0.0), T(+1.0)))
-            # @test isequal(im - T(t), Complex(T(-1.0), T(+1.0)))
+
+            @test isequal(T(+0.0) + im + jm + km, quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(T(-0.0) + im + jm + km, quat(T(-0.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(T(+0.0) - im - jm - km, quat(T(+0.0),T(-1.0),T(-1.0),T(-1.0)))
+            @test isequal(T(-0.0) - im - jm - km, quat(T(-0.0),T(-1.0),T(-1.0),T(-1.0)))
+            @test isequal(T(+1.0) + im + jm + km, quat(T(+1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(T(-1.0) + im + jm + km, quat(T(-1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(T(+1.0) - im - jm - km, quat(T(+1.0),T(-1.0),T(-1.0),T(-1.0)))
+            @test isequal(T(-1.0) - im - jm - km, quat(T(-1.0),T(-1.0),T(-1.0),T(-1.0)))
+            @test isequal(im + jm + km + T(+0.0), quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0)))
+            # Fix these, sign issues with zeros, something wrong with type promotion?
+            # @test isequal(im + jm + km + T(-0.0), quat(T(-0.0),T(+1.0),T(+1.0),T(+1.0)))
+            # @test isequal(im + jm + km - T(+0.0), quat(T(-0.0),T(+1.0),T(+1.0),T(+1.0)))
+            # @test isequal(im + jm + km - T(-0.0), quat(T(-0.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(im + jm + km + T(+1.0), quat(T(+1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(im + jm + km + T(-1.0), quat(T(-1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(im + jm + km - T(+1.0), quat(T(-1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test isequal(im + jm + km - T(-1.0), quat(T(+1.0),T(+1.0),T(+1.0),T(+1.0)))
+            @test T(f) + im + jm + km == quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0))
+            @test T(t) + im + jm + km == quat(T(+1.0),T(+1.0),T(+1.0),T(+1.0))
+            @test T(f) - im - jm - km == quat(T(+0.0),T(-1.0),T(-1.0),T(-1.0))
+            @test T(t) - im - jm - km == quat(T(+1.0),T(-1.0),T(-1.0),T(-1.0))
+            @test im + jm + km + T(f) == quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0))
+            @test im + jm + km + T(t) == quat(T(+1.0),T(+1.0),T(+1.0),T(+1.0))
+            @test im + jm + km - T(f) == quat(T(+0.0),T(+1.0),T(+1.0),T(+1.0))
+            @test im + jm + km - T(t) == quat(T(-1.0),T(+1.0),T(+1.0),T(+1.0))
         end
 
         @testset "multiply" begin
-            @test T(+1.0) * u == u * T(+1.0) == u
+            oq  = quat(T(+1.0),T(+0.0),T(+0.0),T(+0.0))
+            iq  = quat(T(+0.0),T(+1.0),T(+0.0),T(+0.0))
+            jq  = quat(T(+0.0),T(+0.0),T(+1.0),T(+0.0))
+            kq  = quat(T(+0.0),T(+0.0),T(+0.0),T(+1.0))
+            oqn = quat(T(-1.0),T(-0.0),T(-0.0),T(-0.0))
+            iqn = quat(T(-0.0),T(-1.0),T(-0.0),T(-0.0))
+            jqn = quat(T(-0.0),T(-0.0),T(-1.0),T(-0.0))
+            jkn = quat(T(-0.0),T(-0.0),T(-0.0),T(-1.0))
+            @test isequal(T(+1.0) * u,  u)
+            @test isequal(u * T(+1.0),  u)
+            @test isequal(T(-1.0) * u, -u)
+            @test isequal(u * T(-1.0), -u)
+            @test isequal(T(+0.0) * u, quat(T(+0.0),T(+0.0),T(+0.0),T(+0.0)))
+            @test isequal(u * T(+0.0), quat(T(+0.0),T(+0.0),T(+0.0),T(+0.0)))
+            @test isequal(T(-0.0) * u, quat(T(-0.0),T(-0.0),T(-0.0),T(-0.0)))
+            @test isequal(u * T(-0.0), quat(T(-0.0),T(-0.0),T(-0.0),T(-0.0)))
+            @test isequal(oq * oq, oq)
+            @test isequal(iq * iq, quat(T(-1.0),T(+0.0),T(+0.0),T(+0.0)))
+            @test isequal(jq * jq, quat(T(-1.0),T(+0.0),T(+0.0),T(+0.0)))
+            @test isequal(kq * kq, quat(T(-1.0),T(+0.0),T(+0.0),T(+0.0)))
+            @test iq*jq ==  kq == -jq*iq 
+            @test jq*iq == -kq == -iq*jq
+            @test jq*kq ==  iq == -kq*jq
+            @test kq*jq == -iq == -jq*kq
+            @test kq*iq ==  jq == -iq*kq
+            @test iq*kq == -jq == -kq*iq
         end
 
         @testset "divide" begin
@@ -74,3 +111,8 @@ end
     end
 
 end
+
+# @testset "exp(q)" begin
+    
+    
+# end
